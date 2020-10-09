5a4b93d1a35e9a80e3accdf066effe1242d1eea5
Danilo Garcia <dgarcia@citylendinginc.com>
2020-10-08 19:07:55 -0500

    Varias modificaciones realizadas

--- app/jobs/webhook_job.rb
+++ app/jobs/webhook_job.rb
@@ -220,6 +220,48 @@ class WebhookJob < ApplicationJob
           else
             p "ERROR REQUESTING PERSON INFORMATION FROM PIPEDRIVE"    
           end    
+        else
+          
+
+
+          url = "https://api.pipedrive.com/v1/deals/#{deal_id}?api_token=#{ENV['PIPEDRIVE_API_TOKEN']}"
+          uri = URI.parse(url)
+          request = Net::HTTP::Put.new(uri, 'Content-Type' => 'application/json')
+          body = {
+            '8804ec59511693ea1a2789605ebb60634367a164': (loan_guid.nil? ? "" : loan_guid),
+            '39ef775c13d95d43807a1185aa9068a49748646d': (loan_number.nil? ? "" : loan_number),
+            'c4612b19a74ccaa46af6deb7692d83f53bb56a5c': (loan_purpose.nil? ? "" : loan_purpose),
+            'd27f00dd19b57f6edfffed557cf76592aeda93da': (loan_type.nil? ? "" : loan_type),
+            '88135c0b31f5236ac50afb55dc00f3fd6cf6cff5': (interest_rate.nil? ? "" : interest_rate),
+            'f34a68618c73a6336f5c27056b7229bfde08b330': (current_address.nil? ? "" : current_address),
+            '3f4f6458517519d0a98974a4c626bc8264807cec': (subject_property_address.nil? ? "" : subject_property_address),
+            'a4d5d105dda89732508c86905804dc93cd48dbd1': (employer_name.nil? ? "" : employer_name),
+            '4bcd94d9c13feb7a3f2818abaf3a3b77108039fc': (employer_address.nil? ? "" : employer_address),
+            'f4a94170b859ba6cf8ed8eb86745004809eb2cd6': (borrowers_total_income.nil? ? "" : borrowers_total_income),
+            'd7328b1d41abe2be39b7ecc351f2ce61a2fd71a1': (appraisal_value.nil? ? "" : appraisal_value),
+            'bc1276296f056f035541dc45a1de1ade78786fc0': (current_loan_balance.nil? ? "" : current_loan_balance),
+            '3b7f6087cce987adb91868a2415427ff8786c40d': (birthday.nil? ? "" : birthday),
+            'bffb23c1a52bd7b398aad7589f767450b74f4ff2': (closing_date.nil? ? "" : closing_date),
+            'bd0cfc7013f35386da8c2c7d5fbe78c049a1881d': (piti.nil? ? "" : piti),
+            '7494f8dbd3435031ab6925c296912c20573617f4': (ltv.nil? ? "" : ltv)
+          }
+          request.body = JSON.dump(body)
+
+          req_options = {
+            use_ssl: uri.scheme == "https",
+          }
+          
+          response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+            http.request(request)
+          end
+          
+          if response.is_a?(Net::HTTPSuccess)
+            p "CP VARIABLE UPDATED"
+          else
+            p "ERROR UPDATING CP VARIABLE"
+            p response
+            p response.body
+          end
         end
       elsif !is_bulk_update
         stage_id = message_as_json["stage_id"]
@@ -286,7 +328,8 @@ class WebhookJob < ApplicationJob
                 end
                 sw = more_items_in_collection
               else
-                p "ERROR REQUESTING FILES FROM A DEAL FROM PIPEDRIVE"      
+                p "ERROR REQUESTING FILES FROM A DEAL FROM PIPEDRIVE"
+                sw = false
               end
             end             
           end
@@ -294,4 +337,42 @@ class WebhookJob < ApplicationJob
       end
     end
   end
+
+  private
+  def update_cp_variable(person_id)
+    url = "https://api.pipedrive.com/v1/persons/#{person_id}?api_token=#{ENV['PIPEDRIVE_API_TOKEN']}"
+    uri = URI.parse(url)
+    request = Net::HTTP::Get.new(uri)
+    
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+    
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    
+    if response.is_a?(Net::HTTPSuccess)
+      answer = JSON.parse(response.body)
+      data = answer["data"]
+      phones = data["phone"]
+      emails = data["email"]
+
+      items = name_lastname.split(" ")
+
+      if items.size > 1
+        name = items[0]
+        lastname = items[1...items.size].join(" ")
+      else
+        name = name_lastname
+        lastname = ""
+      end
+
+      phone = phones.size > 0 ? phones[0]["value"] : ""
+      email = emails.size > 0 ? emails[0]["value"] : ""
+    else
+      p "ERROR OBTAINING "  
+    end
+  end
+
 end
